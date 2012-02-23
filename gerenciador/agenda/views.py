0600e2b0449d8139765b3ca6aa611039dfc4038f
Roger Camargo <huogerac@gmail.com>
2012-02-23 01:24:30 -0200

    primeiro projeto Django - agendador tarefas

---
+++ gerenciador/agenda/views.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from models import ItemAgenda
+from forms import FormItemAgenda
+
+def lista(request):
+    lista_itens = ItemAgenda.objects.all()
+    return render_to_response("lista.html", {'lista_itens': lista_itens})
+
+def adiciona(request):
+    if request.method == 'POST':
+        form = FormItemAgenda(request.POST, request.FILES)
+        if form.is_valid():
+
+            dados = form.cleaned_data
+            item = ItemAgenda(data=dados['data'],
+                hora=dados['hora'], 
+                titulo=dados['titulo'], 
+                descricao=dados['descricao'])
+            item.save()
+
+            return render_to_response('salvo.html', {})
+
+    else:
+        form = FormItemAgenda()
+
+    return render_to_response("adiciona.html", {'form': form}, context_instance=RequestContext(request))
+
+
+
+
