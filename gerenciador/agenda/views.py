e87c3f80a819a4fce47ec3eddd7ebef324bdb9fd
Roger Camargo <huogerac@gmail.com>
2012-02-27 23:37:29 -0300

    utilizando ModelForms para atualizar registros

--- gerenciador/agenda/views.py
+++ gerenciador/agenda/views.py
@@ -16,24 +16,24 @@ def adiciona(request):
     if request.method == 'POST':
         form = FormItemAgenda(request.POST, request.FILES)
         if form.is_valid():
-
-            dados = form.cleaned_data
-            item = ItemAgenda(data=dados['data'],
-                hora=dados['hora'],
-                titulo=dados['titulo'],
-                descricao=dados['descricao'])
-            item.save()
-
+            form.save()
             return render_to_response('salvo.html', {})
 
     else:
         form = FormItemAgenda()
 
-    return render_to_response("adiciona.html", {'form': form}, context_instance=RequestContext(request))
+    return render_to_response("item.html", {'form': form}, context_instance=RequestContext(request))
 
 
 def item(request, nr_item):
     item = get_object_or_404(ItemAgenda, pk=nr_item)
-    return render_to_response('item.html', {'item': item})
+    if request.method == "POST":
+        form = FormItemAgenda(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            return render_to_response("salvo.html", {})
+    else:
+        form = FormItemAgenda(instance=item)
+    return render_to_response('item.html', {'form': form}, context_instance=RequestContext(request))
 
 
